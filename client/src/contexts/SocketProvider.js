1472bb4328ab5e69888a5d62b771dbcf0cde5f02
JeremiahISamuelMMS <79987177+JeremiahISamuelMMS@users.noreply.github.com>
2021-12-29 23:03:53 -0500

    Signed-off-by: JeremiahISamuelMMS <79987177+JeremiahISamuelMMS@users.noreply.github.com>

--- client/src/contexts/SocketProvider.js
+++ client/src/contexts/SocketProvider.js
@@ -12,7 +12,7 @@ export function SocketProvider({ id, children }) {
 
   useEffect(() => {
     const newSocket = io(
-      'http://localhost:5000',
+      'https://jis-whatsapp-server.vercel.app',
       { query: { id } }
     )
     setSocket(newSocket)
